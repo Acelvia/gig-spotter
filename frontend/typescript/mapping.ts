f8872cf42fa2f229e7c4baa520a14fdbe6cffed7
Markus Yrjölä <yrjola.markus@gmail.com>
2015-10-25 01:34:29 +0300

    Took the MarkerClusterer library to use which creates clusters from individual markers on the map. Increased the amount of results fetched from the API tp 50 to get more interesting clusters.

--- frontend/typescript/mapping.ts
+++ frontend/typescript/mapping.ts
@@ -1,7 +1,7 @@
-/// <reference path="./google.maps.d.ts" />
+/// <reference path="./typings/google.maps.d.ts" />
+/// <reference path="./typings/markerclustererplus.d.ts" />
 import {Http, HTTP_PROVIDERS} from 'angular2/http';
-import {Component, View, bootstrap} from 'angular2/angular2';
-import {CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/angular2';
+import {bootstrap, Component} from 'angular2/angular2';
 
 @Component({
     selector: 'map',
@@ -9,30 +9,35 @@ import {CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/angular2';
 })
 class GoogleMap {
 
-    map;
-    markers = [];
+    markerClusterer;
     latitude = 60.1733343;
     longitude = 24.93227;
 
     constructor(public http: Http) {
         var self = this;
 
-        this.map = new google.maps.Map(document.getElementById('mapCanvas'), {
+        var map = new google.maps.Map(document.getElementById('mapCanvas'), {
             // Center on Helsinki
             center: new google.maps.LatLng(this.latitude, this.longitude),
             zoom: 13
         });
+        this.markerClusterer = new MarkerClusterer(map, [], {
+            gridSize: 200,
+            zoomOnClick: false,
+            averageCenter: true,
+            minimumClusterSize: 1,
+            enableRetinaIcons: true,
+        });
 
         // Update the map when the user is not interacting with it,
         // including when the map has finished loading.
-        google.maps.event.addListener(this.map, 'idle', function() {
-            var mapBounds = self.map.getBounds();
+        google.maps.event.addListener(map, 'idle', function() {
+            var mapBounds = self.markerClusterer.getMap().getBounds();
             var neLat = mapBounds.getNorthEast().lat(),
                 neLng = mapBounds.getNorthEast().lng(),
                 swLat = mapBounds.getSouthWest().lat(),
                 swLng = mapBounds.getSouthWest().lng();
 
-            //http.get('/accommodation?latitude=' + self.latitude + '&longitude=' + self.longitude)
             http.get('/accommodation?neLat=' + neLat + '&neLng=' + neLng + '&swLat=' + swLat + '&swLng=' + swLng)
                 .map(result => result.json())
                 .subscribe(accommodations => self.renderAccommodations(accommodations));
@@ -40,23 +45,11 @@ class GoogleMap {
     }
 
     renderAccommodations(accommodations) {
-        var self = this;
-        this.deleteMarkers();
-        accommodations.result.map(function(result) {
-            return new google.maps.LatLng(result.latLng[0], result.latLng[1]);
-        }).forEach(function(latLng) {
-            self.markers.push(new google.maps.Marker({
-                position: latLng,
-                map: self.map
-            }));
-        });
-    }
-
-    deleteMarkers() {
-        for (var i = 0; i < this.markers.length; i++) {
-            this.markers[i].setMap(null);
-        }
-        this.markers = [];
+        this.markerClusterer.clearMarkers();
+        this.markerClusterer.addMarkers(accommodations.result.map(function(result) {
+            var latLng = new google.maps.LatLng(result.latLng[0], result.latLng[1]);
+            return new google.maps.Marker({ position: latLng });
+        }));
     }
 }
 
