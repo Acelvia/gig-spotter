a1dd71ba5eda97853f290540c61ca75ff305ef09
Mattia <mattia.denti@acelvia.com>
2015-10-24 16:00:22 +0300

    simplified frontend scripts and process

--- frontend/typescript/test.ts
+++ frontend/typescript/test.ts
@@ -1,7 +1,22 @@
-import {Component, bootstrap} from 'angular2/angular2';
+import {Component, bootstrap, FORM_DIRECTIVES} from 'angular2/angular2';
+
+class Search {
+  job: string;
+  city: string;
+  apartment: string;
+}
+
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+  selector: 'my-app',
+  templateUrl: 'templates/test.html',
+  directives: [FORM_DIRECTIVES]
 })
-class AppComponent { }
+class AppComponent {
+  public search: Search = {
+    job: 'job there',
+    city: 'city here',
+    apartment: 'apartment here'
+  };
+}
+
 bootstrap(AppComponent);
